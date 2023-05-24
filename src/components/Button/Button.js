ec5bd720718c44c326a65410c2e08fbb5f214200
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-05-24 14:12:13 +0300

    change button position

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
-import { LoadMore } from './Button.styled';
+import { LoadMore,ButtonContainer } from './Button.styled';
 
 export const Button = ({ onClick }) => {
-  return <LoadMore onClick={onClick}>Load More</LoadMore>;
+  return (<ButtonContainer><LoadMore onClick={onClick}>Load More</LoadMore></ButtonContainer>);
 };
 
 Button.propTypes = {
--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -25,5 +25,10 @@ export const LoadMore = styled.button`
   min-width: 180px;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-  transform: translateX(675px);
 `;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
